1320c8eb23edb99b9d4f543002b0915792bb43d7
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2024-08-09 22:07:15 +0530

    Update Find_String_Roots.cpp

--- STRING ALGORITHMS/Find_String_Roots.cpp
+++ STRING ALGORITHMS/Find_String_Roots.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+	
+	while(true){
+		
+		string s;
+		cin >> s;
+		if(s=="*") break;
+		int n = s.length();
+		vector<int> lps(n,0);
+		
+		int i = 1, len = 0;
+		
+		while(i<n){
+			if(s[i]==s[len]){
+				len++;
+				lps[i]=len;
+				i++;
+			}
+			else{
+				if(len>0){
+					len = lps[len-1];
+				}
+				else{
+					i++;
+				}
+			}
+		}
+		
+		int lps_max = lps[n-1];
+		
+		if(n%(n-lps_max)==0){
+			cout << n/(n-lps_max) << endl;
+		}
+		else{
+			cout << 1 << endl;
+		}
+	}
+
+	return 0;
+}
